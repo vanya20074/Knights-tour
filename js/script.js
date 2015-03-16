bb20e7ff4f4d7d749f9414c4d45fd75f42529ac1
vanya <vanya20074@gmail.com>
2015-03-16 11:07:46 +0200

    add comment before some functions

--- js/script.js
+++ js/script.js
@@ -23,6 +23,9 @@ function init(rows, columns) {
             $('.grid-container').empty();
         }
 
+        /**
+         * Generate grid with parameters from init(rows, columns)
+         */
         function generage_grid() {
             for (var i = 0; i < rows; i++) {
                 $('.grid-container').append('<div class="grid-row"></div>');
@@ -118,7 +121,9 @@ function init(rows, columns) {
 
         });
 
-        //field shading
+        /**
+         * Field shading function, add black and white class to cell
+         */
         function shading() {
             var $cells = $('.grid-cell');
             $cells.each(function () {
@@ -138,12 +143,11 @@ function init(rows, columns) {
 
         shading();
 
-//    $('.restart-button').click(function () {
-//        location.reload();
-//    });
-
+        /**
+         * Restart game with new grid that set in init()
+         */
         $('.restart-button').click(function () {
-            init(10,10);
+            init(10, 10);
             var $cells = $('.grid-cell');
             $cells.each(function () {
                 $(this).fadeOut(900, function () {
@@ -154,56 +158,44 @@ function init(rows, columns) {
                 })
 
                 $i = 0;
-                $n = 0;
+
                 firstStepFlag = 1;
 
             });
             $('#fade , #game_message_win, #game_message_lose').fadeOut(900);
 
         });
+        /**
+         * Generate popup window after win or lose
+         *
+         * @param message_id get game_message_win or game_message_lose
+         */
         function getPopUp(message_id) {
 
-// Here we will describe a variable popupid which gets the
-// rel attribute from the clicked link
             var popupid = message_id;
             console.log(popupid);
 
-// Now we need to popup the marked which belongs to the rel attribute
-// Suppose the rel attribute of click link is popuprel then here in below code
-// #popuprel will fadein
             $('#' + popupid).fadeIn(900);
 
-
-// append div with id fade into the bottom of body tag
-// and we allready styled it in our step 2 : CSS
-//$('body').append('<div id="fade"></div>');
             $('#fade').css({'filter': 'alpha(opacity=80)'}).fadeIn(900);
 
-
-// Now here we need to have our popup box in center of
-// webpage when its fadein. so we add 10px to height and width
             var popuptopmargin = ($('#' + popupid).height() + 10) / 2;
             var popupleftmargin = ($('#' + popupid).width() + 10) / 2;
 
-
-// Then using .css function style our popup box for center allignment
             $('#' + popupid).css({
                 'margin-top': -popuptopmargin,
                 'margin-left': -popupleftmargin
             });
 
-            // Now define one more function which is used to fadeout the
-// fade layer and popup window as soon as we click on fade layer
             $('#fade').click(function () {
-
-
-// Add markup ids of all custom popup box here
                 $('#fade , #game_message_win, #game_message_lose').fadeOut(900)
                 return false;
-
             });
         }
 
+        /**
+         * Language select
+         */
         $('#en').click(function () {
             console.log('change to en');
             $('#lang').text('English');
@@ -220,7 +212,7 @@ function init(rows, columns) {
 
     });
 }
-init(15,10);
+init(15, 10);
 
 
 
